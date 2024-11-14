2456f9c66fc5b390f7319230e78452254d8b7acf
Christopher Makousky <christopher.makousky@gmail.com>
2024-11-14 05:38:05 -0600

    started work on inquirer prompts

--- src/db/index.ts
+++ src/db/index.ts
@@ -23,9 +23,45 @@ export default class DB {
     }
 
      // view all roles
+     viewRoles() {
+        return this.query(
+            "SELECT id, title, salary, department_id FROM roles"
+        );
+    }
+
      // view all employees
+     viewEmployees() {
+        return this.query(
+            "SELECT id, first_name, last_name, role_id, manager_id FROM employee"
+        );
+    }
+    
      // add a department
+    addDepartment(department_name: string) {
+        return this.query(
+            "INSERT INTO department(department_name) VALUES ($1)", [department_name]
+        );
+    }
+
      // add a role
+     addRole(title: string, salary: number, department_id: number) {
+        return this.query(
+            "INSERT INTO role(title, salary, department_id) VALUES ($1, $2, $3)", [title, salary, department_id]
+        );
+     }
+
      // add an employee
+     addEmployee(first_name: string, last_name: string, role_id: number, manager_id: number) {
+        return this.query(
+            "INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3)", 
+            [first_name, last_name, role_id, manager_id]
+        );
+     }
+
      // update an employee role
+     updateEmployeeRole(id: number, role_id: number) {
+        return this.query(
+            "UPDATE employee SET role_id = $2 WHERE id = $1", [id, role_id]
+        );
+     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,133 @@
+import inquirer from "inquirer";
+import logo from "asciiart-logo";
 import DB from "./db/index.js";
 
 const db = new DB();
 
-db.viewDepartments()
-    .then(({ rows }) => {console.table(rows)});
+function loadMainPrompts() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                {
+                    name: 'View all departments',
+                    value: 'VIEW_DEPARTMENTS'
+                },
+                {
+                    name: 'View all roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
+                    name: 'View all employees',
+                    value: 'VIEW_EEMPLOYEES'
+                },
+                {
+                    name: 'Add a department',
+                    value: 'ADD_DEPARTMENT'
+                },
+                {
+                    name: 'Add a role',
+                    value: 'ADD_ROLE'
+                },
+                {
+                    name: 'Add an employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name: 'Udate an employee role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
+                },
+            ]
+        }
+    ])
+    .then(res => {
+        const choice = res.choice;
+        switch(choice) {
+            case 'VIEW_DEPARTMENTS':
+                viewDepartments();
+                break;
+            case 'VIEW_ROLES':
+                viewRoles();
+                break;
+            case 'VIEW_EMPLOYEES':
+                viewEmployees();
+                break;
+            case 'ADD_DEPARTMENT':
+                addDepartment();
+                break;
+            case 'ADD_ROLE':
+                addRole();
+                break;
+            case 'ADD_EMPLOYEE':
+                addEmployee();
+                break;
+            case 'UPDATE_EMPLOYEE_ROLE':
+                updateEmployeeRole();
+                break;
+            default: quit();
+        }
+    })
+};
+
+function viewDepartments() {
+    db.viewDepartments()
+    .then(({ rows }) => {
+        const departments = rows;
+        console.log(`\n`);
+        console.table(departments);
+    })
+    .then(() => loadMainPrompts());
+};
+
+function viewRoles() {
+    db.viewRoles()
+    .then(({ rows }) => {
+        const roles = rows;
+        console.log(`\n`);
+        console.table(roles);
+    })
+    .then(() => loadMainPrompts());
+};
+
+function viewEmployees() {
+    db.viewEmployees()
+    .then(({ rows }) => {
+        const employees = rows;
+        console.log(`\n`);
+        console.table(employees);
+    })
+    .then(() => loadMainPrompts());
+};
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'department_name',
+            message: 'What is the department name?',
+            type: 'input'
+        }
+    ])
+    .then(resp => {})
+};
+
+function addRole() {};
+
+function addEmployee() {};
+
+function updateEmployeeRole() {};
+
+function quit() {};
+
+function init() {
+    const logoText = logo({ name: "Employee Tracker" });
+    console.log(logoText);
+
+    loadMainPrompts();
+};
+
+// db.viewDepartments()
+//     .then(({ rows }) => {console.table(rows)});
+
+init();
