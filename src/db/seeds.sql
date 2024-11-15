045b90cc1dc32e770cf44fff27bdee3d264b3c26
Christopher Makousky <christopher.makousky@gmail.com>
2024-11-15 13:34:17 -0600

    updated seeds.sql

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -14,6 +14,6 @@ VALUES  ('King', 10000000.00, 1),
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES  ('Jack', 'Atlas', 1, NULL),
-        ('Joeseph', 'Joestar', 2, 4),
+        ('Joseph', 'Joestar', 2, 4),
         ('Caesar', 'Zeppeli', 2, 4),
         ('Lisa', 'Lisa', 4, NULL);
